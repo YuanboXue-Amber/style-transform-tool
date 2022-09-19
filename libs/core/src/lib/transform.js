5ef64011a4652a0ef816c358b990332de50d7a2f
YuanboXue-Amber <yuanboxue@microsoft.com>
2022-09-19 19:01:21 +0200

    UT

--- libs/core/src/lib/transform.js
+++ libs/core/src/lib/transform.js
@@ -2,20 +2,25 @@ import * as Babel from '@babel/standalone';
 import { hasToken, tokensV0toV9 } from './transformToken';
 import { transformShorthandsPlugin } from 'v9helper-babel-plugin-shorthands';
 
-const transformTokenPlugin = () => {
+const transformTokenPlugin = ({ types: t }) => {
   return {
     visitor: {
       StringLiteral(path) {
         if (path.node.value && hasToken(path.node.value)) {
           const replacementResult = tokensV0toV9(path.node.value);
-          path.replaceWithSourceString(replacementResult.value);
 
           if (replacementResult.comments.length) {
             // found token without matching in v9, add comments for them
             replacementResult.comments.reverse().forEach((comment) => {
-              path.parentPath.addComment('leading', comment, true);
+              let parent = path.parentPath;
+              if (t.isSpreadElement(parent.parentPath)) {
+                parent = parent.parentPath;
+              }
+              parent?.addComment('leading', comment, true);
             });
           }
+
+          path.replaceWithSourceString(replacementResult.value);
         }
       },
     },
